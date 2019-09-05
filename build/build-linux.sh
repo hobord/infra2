9f036c3d041622edbf2c1861d2c5662bee362a76
Balázs Szabó <balazs.szabo@gmail.com>
2019-09-05 09:42:43 +0300

    cmd's dir struct

--- build/build-linux.sh
+++ build/build-linux.sh
@@ -1,4 +1,4 @@
-env GOOS=linux GOARCH=amd64 go build -o bin/linux/sessionServer cmd/session.go
-env GOOS=linux GOARCH=amd64 go build -o bin/linux/redirectServer cmd/redirect.go
-env GOOS=linux GOARCH=amd64 go build -o bin/linux/infra2Server cmd/infra2.go
+env GOOS=linux GOARCH=amd64 go build -o bin/linux/sessionServer cmd/session/session.go
+env GOOS=linux GOARCH=amd64 go build -o bin/linux/redirectServer cmd/redirect/redirect.go
+env GOOS=linux GOARCH=amd64 go build -o bin/linux/infra2Server cmd/infra/infra2.go
 
